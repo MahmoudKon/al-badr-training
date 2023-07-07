283928c302254d770cbb812ebf73b7e405feaf63
Mahmoud Mohammed <mahmoud.mohammed050684@gmail.com>
2023-07-07 16:35:01 +0300

    add auth laravel plugin and make publish for stubs folder

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,14 +3,20 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\Hash;
+use App\Models\Scopes\PerShopScope;
 use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable, SoftDeletes;
 
     /**
      * The attributes that are mass assignable.
@@ -42,4 +48,30 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    protected function password(): Attribute
+    {
+        return Attribute::make(
+            set: fn ($value) => Hash::make($value),
+        );
+    }
+
+    public function scopeFilter(Builder $builder): Builder
+    {
+        return $builder;
+    }
+
+    protected static function booted(): void
+    {
+        static::addGlobalScope(new PerShopScope); // assign the Scope here
+    }
+
+    protected static function boot(): void
+    {
+        parent::boot();
+
+        self::creating(function($model) {
+            $model->shop_id = auth()->user()->shop_id;
+        });
+    }
 }
